00cc438a4519392470ea310ab233b6629cb4337b
David West <59415168+dwest1507@users.noreply.github.com>
2021-01-25 10:39:35 -0500

    Created apps to analyze the gapminder dataset.

---
+++ shinyapps/gapminder_app.R
@@ -0,0 +1,47 @@
+library(shiny)
+library(tidyverse)
+library(colourpicker)
+library(gapminder)
+library(plotly)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      textInput("title", "Title", "GDP vs life exp"),
+      numericInput("size", "Point size", 1, 1),
+      checkboxInput("fit", "Add line of best fit", FALSE),
+      colourInput("color", "Point color", "blue"),
+      selectInput("continents", "Continents",
+                  choices = levels(gapminder$continent),
+                  multiple = TRUE,
+                  selected = "Europe"),
+      sliderInput("years", "Years",
+                  min(gapminder$year), max(gapminder$year),
+                  value = c(1977, 2002))
+    ),
+    mainPanel(
+      plotlyOutput("plot")
+    )
+  )
+)
+
+# Define the server logic
+server <- function(input, output) {
+  output$plot <- renderPlotly({
+    data <- subset(gapminder,
+                   continent %in% input$continents &
+                     year >= input$years[1] & year <= input$years[2])
+    
+    p <- ggplot(data, aes(gdpPercap, lifeExp)) +
+      geom_point(size = input$size, col = input$color) +
+      scale_x_log10() +
+      ggtitle(input$title)
+    
+    if (input$fit) {
+      p <- p + geom_smooth(method = "lm")
+    }
+    ggplotly(p)
+  })
+}
+
+shinyApp(ui = ui, server = server)
---
+++ shinyapps/gapminder_app2.R
@@ -0,0 +1,31 @@
+library(shiny)
+library(gapminder)
+
+ui <- fluidPage(
+  h1("Gapminder"),
+  sliderInput(inputId = "life", label = "Life expectancy",
+              min = 0, max = 120,
+              value = c(30, 50)),
+  selectInput("continent", "Continent",
+              choices = c("All", levels(gapminder$continent))),
+  tableOutput("table")
+)
+
+server <- function(input, output) {
+  output$table <- renderTable({
+    data <- gapminder
+    data <- subset(
+      data,
+      lifeExp >= input$life[1] & lifeExp <= input$life[2]
+    )
+    if (input$continent!="All") {
+      data <- subset(
+        data,
+        continent == input$continent
+      )
+    }
+    data
+  })
+}
+
+shinyApp(ui, server)
